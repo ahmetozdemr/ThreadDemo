76cab160a1d2ebf5fc5929f88f90ca9beae8d83c
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-05-16 23:30:47 +0300

    add new thread

---
+++ ThreadMonitor/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    private static object _lockObject = new object();
+
+    public static void Main(string[] args)
+    {
+        // Kritik bölüme aynı anda erişmek için birden çok thread oluşturun
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(DoWork);
+            thread.Start(i);
+        }
+
+        Console.ReadLine();
+    }
+
+    static void DoWork(object threadId)
+    {
+        Console.WriteLine("Thread {0} kritik bölüme girmeye çalışıyor.", threadId);
+
+        // Kritik bölüme erişmek için lock nesnesini kullanın
+        lock (_lockObject)
+        {
+            try
+            {
+                Console.WriteLine("Thread {0} kritik bölüme girdi ve bazı işlemler gerçekleştiriyor.", threadId);
+                Thread.Sleep(2000); // Biraz iş simülasyonu
+
+                Console.WriteLine("Thread {0} kritik bölümden çıkıyor.", threadId);
+            }
+            finally
+            {
+                // Kilit nesnesini serbest bırakın
+            }
+        }
+    }
+}
---
+++ ThreadMutex/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    private static Mutex _mutex = new Mutex();
+    public static void Main(string[] args)
+    {
+        // Kritik bölüme aynı anda erişmek için birden çok thread oluşturun
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(DoWork);
+            thread.Start(i);
+        }
+
+        Console.ReadLine();
+    }
+
+    static void DoWork(object threadId)
+    {
+        Console.WriteLine("Thread {0} kritik bölüme girmeye çalışıyor.", threadId);
+
+        // Mutex'in müsait olmasını bekleyin
+        _mutex.WaitOne();
+
+        try
+        {
+            Console.WriteLine("Thread {0} kritik bölüme girdi ve bazı işlemler gerçekleştiriyor.", threadId);
+            Thread.Sleep(2000); // Biraz iş simülasyonu
+
+            Console.WriteLine("Thread {0} kritik bölümden çıkıyor.", threadId);
+        }
+        finally
+        {
+            // Mutex'i serbest bırakın
+            _mutex.ReleaseMutex();
+        }
+    }
+}
