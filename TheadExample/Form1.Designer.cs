dbce9df03bb70f674311d7420fa9f30fd2d291eb
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-05-16 15:50:42 +0300

    first commit

--- TheadExample/Form1.Designer.cs
+++ TheadExample/Form1.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel4 = new Panel();
+            textBox1 = new TextBox();
+            panel3 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            buttonlistBox = new Button();
+            listBox1 = new ListBox();
+            panel1 = new Panel();
+            buttonProgres = new Button();
+            progressBar1 = new ProgressBar();
+            panel4.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.MenuHighlight;
+            panel4.Controls.Add(textBox1);
+            panel4.Location = new Point(429, 265);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(289, 68);
+            panel4.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(11, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(270, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.GrayText;
+            panel3.Controls.Add(pictureBox1);
+            panel3.Location = new Point(445, 24);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(265, 163);
+            panel3.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(16, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(232, 110);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Yellow;
+            panel2.Controls.Add(buttonlistBox);
+            panel2.Controls.Add(listBox1);
+            panel2.Location = new Point(31, 173);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(233, 201);
+            panel2.TabIndex = 11;
+            // 
+            // buttonlistBox
+            // 
+            buttonlistBox.Location = new Point(28, 15);
+            buttonlistBox.Name = "buttonlistBox";
+            buttonlistBox.Size = new Size(176, 29);
+            buttonlistBox.TabIndex = 3;
+            buttonlistBox.Text = "ListBox Yazdır";
+            buttonlistBox.UseVisualStyleBackColor = true;
+            buttonlistBox.Click += buttonlistBox_Click_1;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(28, 50);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(176, 144);
+            listBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 128, 255);
+            panel1.Controls.Add(buttonProgres);
+            panel1.Controls.Add(progressBar1);
+            panel1.Location = new Point(31, 24);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(312, 125);
+            panel1.TabIndex = 10;
+            // 
+            // buttonProgres
+            // 
+            buttonProgres.Location = new Point(25, 25);
+            buttonProgres.Name = "buttonProgres";
+            buttonProgres.Size = new Size(227, 29);
+            buttonProgres.TabIndex = 0;
+            buttonProgres.Text = "Progres Bar Doldur";
+            buttonProgres.UseVisualStyleBackColor = true;
+            buttonProgres.Click += buttonProgres_Click_1;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(25, 78);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(278, 29);
+            progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel4;
+        private TextBox textBox1;
+        private Panel panel3;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private Button buttonlistBox;
+        private ListBox listBox1;
+        private Panel panel1;
+        private Button buttonProgres;
+        private ProgressBar progressBar1;
     }
 }
--- TheadExample/Form1.cs
+++ TheadExample/Form1.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace TheadExample
 {
     public partial class Form1 : Form
@@ -5,6 +7,111 @@ namespace TheadExample
         public Form1()
         {
             InitializeComponent();
+            System.Windows.Forms.Form.CheckForIllegalCrossThreadCalls = false;//windows bazen kendi oluþturduðumuz thread'lere izin vermiyor bu engeli aþmak için
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread resimDonder = new Thread(resimCekici);//göstereceðimiz resimler için oluþturduðumuz thread
+            resimDonder.Start();//thread baþlatma
+
+
+            Thread textDondur = new Thread(GetText);
+            textDondur.Start();
+        }
+        private void resimCekici()//resimleri siteden çekmek için yazdýðýmýz thread fonksiyonu
+        {
+            while (true)//resimler picture box'ýmýzda 1 er saniye ara ile gösteriliyor
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;//gelen resmi picture box'ýn boyutuna göre ölçeklendiriyoruz
+                pictureBox1.Image = resimGetir(@"https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/19005252/original/5a12046d74ffa8c53c4eb6334894f0fcca967f39/make-your-piture-with-neon-glow-effect.jpg");
+                Thread.Sleep(1000);//thread fonksiyonunu 1 saniye duraklatýp diðer resmi çekiyoruz
+                pictureBox1.Image = resimGetir(@"https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs2/19005252/original/57ddbc9199a432b4778e5aba3d24235e8ba86fc0/make-your-piture-with-neon-glow-effect.jpg");
+                Thread.Sleep(1000);
+                pictureBox1.Image = resimGetir(@"https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs3/19005252/original/967de46b83b470c48ef255290a25a0ab4f93aa82/make-your-piture-with-neon-glow-effect.jpg");
+                Thread.Sleep(1000);
+                pictureBox1.Image = resimGetir(@"https://fiverr-res.cloudinary.com/images/t_smartwm/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/02cd649322c3ddc2367d1ddfe87413d7-1600171415/fff/make-your-piture-with-neon-glow-effect.jpg");
+                Thread.Sleep(1000);
+            }
+        }
+
+        Bitmap resimGetir(String url)//siteden gelen url ile resimler çekiliyor
+        {
+            WebRequest resimIstek = WebRequest.Create(url);//url için istek yapýyoruz
+            return (Bitmap)Bitmap.FromStream(resimIstek.GetResponse().GetResponseStream());//geri dönen resmi resimCekici fonksiyonuna gönderiyoruz
+        }
+        private void GetText()
+        {
+            while (true)
+            {
+                textBox1.Text = "Tavþan";
+                Thread.Sleep(1000);
+                textBox1.Text = "Tilki";
+                Thread.Sleep(1000);
+                textBox1.Text = "Kaplumbaða";
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void buttonProgres_Click(object sender, EventArgs e)
+        {
+            Thread progresDoldurThread = new Thread(progresDoldurucu);//Ýlk thread'imiz progres bar doldurmak için 
+            progresDoldurThread.Start();//Thread'i baþlatýyoruz
+        }
+        private void progresDoldurucu()//progresDoldurThread'i için yazdýðýmýz fonksiyon
+        {
+            while (true)//progres bar dolana kadar dönen döngümüz
+            {
+                progressBar1.Maximum = 100;//progres bar'ýn maximum deðeri
+                if (progressBar1.Value == 90)
+                {
+                    progressBar1.Value = 0;
+                }
+                progressBar1.Value += 10;//deðerinin kaçar kaçar artacaðý
+                Thread.Sleep(1000);//fonksiyonun bekleme süresi mili saniye cinsinden
+            }
+
+        }
+
+        private void buttonlistBox_Click(object sender, EventArgs e)
+        {
+            Thread listBoxThread = new Thread(listBoxDoldur);//listbox'ý&amp;amp;nbsp; doldurmak için kullandýðýmýz thread
+            listBoxThread.Start();//thread baþlatýlýyor
+        }
+        private void listBoxDoldur()//thread fonksiyonumuz
+        {
+            while (true)
+            {
+                listBox1.Items.Add("Ahmet Özdemir");//thread çalýþýnca sürekli listbox'a ekliyor
+                Thread.Sleep(500);//yarým saniye bekleme süresi
+            }
+        }
+
+        private void buttonProgres_Click_1(object sender, EventArgs e)
+        {
+            Thread progresDoldurThread = new Thread(progresDoldurucu);//Ýlk thread'imiz progres bar doldurmak için 
+            progresDoldurThread.Start();//Thread'i baþlatýyoruz
+        }
+
+        private void buttonlistBox_Click_1(object sender, EventArgs e)
+        {
+            Thread listBoxThread = new Thread(listBoxDoldur);//listbox'ý&amp;amp;nbsp; doldurmak için kullandýðýmýz thread
+            listBoxThread.Start();//thread baþlatýlýyor
         }
     }
 }
+
+/* Thread oluþturma süreci
+System.Windows.Forms.Form.CheckForIllegalCrossThreadCalls = false;//windows bazen kendi oluþturduðumuz thread'lere izin vermiyor bu engeli aþmak için 
+using System.Threading;//thread kullanýmý için eklmemiz gereken kütüphane
+Thread thread = new Thread(threadFonk);//burada bir fonksiyon ismi yazacaðýz thread bu fonksiyonu çalýþtýracak
+thread.Start();//thread'i baþlatmamýz gerekir 
+thread.Suspend();//thread'i durdurur ama yok edemez sadece susturur
+thread.Resume();//duran thread'i geri çalýþtýrýr 
+void threadFonk()//çalýþacak fonksiyon
+{
+    ....çalýþacak iþlemler
+    Thread.Sleep(1000);//thread'i yavaþlatmak istersek kullanacaðýmýz fonksiyon
+
+}
+*/
